810476bae4e87f949d55f8947fb5f920f8dd1b1d
Raghu Betina <student@wireless-193-221.uchicago.edu>
2014-10-28 13:09:47 -0500

    Switched back to not using each in one action

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,6 +1,10 @@
 class FortunesController < ApplicationController
   def lucky_numbers
-    @numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
+    @first_number = rand(100)
+    @second_number = rand(100)
+    @third_number = rand(100)
+    @fourth_number = rand(100)
+    @fifth_number = rand(100)
   end
 
   def unlucky_numbers
