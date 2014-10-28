c83a731f1f5132cc128092c1694283da79338c0a
rbetina <rbetina@Raghus-MacBook-Pro.local>
2014-10-28 09:30:46 -0500

    Switched to .each in lucky_numbers view

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,9 +1,5 @@
 class FortunesController < ApplicationController
   def numbers
-    @first_number = rand(1..100)
-    @second_number = rand(1..100)
-    @third_number = rand(1..100)
-    @fourth_number = rand(1..100)
-    @fifth_number = rand(1..100)
+    @lucky_numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
   end
 end
